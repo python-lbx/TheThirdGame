3d12ca7047e5620e1db778a0da745f5bcb0468ee
Fong <60109720+python-lbx@users.noreply.github.com>
2023-07-03 23:52:47 +0800

    update
    sign start

--- TheThirdGame/Assets/Script/TutorialRoom/Movement_Tutorial.cs
+++ TheThirdGame/Assets/Script/TutorialRoom/Movement_Tutorial.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement_Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject[] UI;
+    public GameObject UIBG;
+    public int num;
+    public bool here;
     // Update is called once per frame
     void Update()
     {
-        
+        if(here)
+        {
+            UIBG.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.Y) && num < UI.Length)
+            {
+                num ++;
+            }
+
+            
+            if(num >= UI.Length)
+            {
+                num = 0;
+            }
+
+            for(int i = 0 ; i < UI.Length ; i++)
+            {
+                if(i == num)
+                {
+                    UI[i].SetActive(true);
+                }
+                else
+                {
+                    UI[i].SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            UIBG.SetActive(false);
+            
+            for(int i = 0 ; i < UI.Length ; i++)
+            {
+                UI[i].SetActive(false);
+                num = 0;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            here = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            here = false;
+        }
     }
 }
